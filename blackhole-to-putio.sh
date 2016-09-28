45f96bdc852165e1c378e65708948de264525d4d
Adrien Anceau <adrien.anceau@gmail.com>
2016-09-28 11:31:27 +0200

    Transfers are now tracked

--- blackhole-to-putio.sh
+++ blackhole-to-putio.sh
@@ -26,6 +26,12 @@ do
 	id=$(jq -r .transfer.id <<< $result)
 	print_success "Transfer added with id $id"
 
+	# Creating file for transfer tracking
+	touch $BLACKHOLE/${id}.transfer
+
+	# Marking torrent as processed
+	mv $file ${file}.$(date +"%Y-%m-%d_%H-%M").processed
+
 	if [[ -n $SLACK_WEBHOOK_URL ]]; then
 		print_info "Sending notification to Slack"
 		send_slack_notification "Torrent $(basename ${file}) sent to Put.io"
@@ -33,3 +39,39 @@ do
 	echo
 
 done
+
+print_info "Retrieving completed downloads list ..."
+completedDownloadsJson=$(curl --silent "https://api.put.io/v2/files/list?oauth_token=${PUTIO_TOKEN}&parent_id=${FOLDER_ID}")
+
+declare -a retrieveList
+oIFS=$IFS
+IFS=$'\n'
+for line in $( jq -r '.files[] | (.id | tostring) + ":" + .name' <<< $completedDownloadsJson)
+do
+	dlId=$(cut -d':' -f1 <<< $line)
+	dlName=$(cut -d':' -f2 <<< $line)
+
+	# Checking if file is one we need
+	weNeedThisFile=0
+	[[ -f $BLACKHOLE/${dlId}.transfer ]] && weNeedThisFile=1
+
+	[[ $weNeedThisFile == 1 ]] && retrieveList+=("$dlId")
+
+	color=$YELLOW
+	[[ $weNeedThisFile == 1 ]] && color=$GREEN 
+	print_info "    - ${BOLD}$dlId${END} : ${color}$dlName${END}"
+done
+IFS=$oIFS
+
+echo
+if [[ -n ${retrieveList[0]} ]]; then
+	print_info "Downloading watched transfers ..."
+
+	for id in ${retrieveList[@]}
+	do
+		echo
+		aria2c "http://api.put.io/v2/files/426556096/download?oauth_token=${PUTIO_TOKEN}"
+	done
+else
+	print_info "No watched transfer is finished, nothing to download"
+fi
