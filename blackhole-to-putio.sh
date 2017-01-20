9986514e0c502794d4736022825d32af54310e95
Adrien Anceau <adrien.anceau@gmail.com>
2017-01-20 10:37:00 +0100

    blackhole to putio update

--- blackhole-to-putio.sh
+++ blackhole-to-putio.sh
@@ -3,6 +3,7 @@
 source common.sh
 
 [[ -z $BLACKHOLE ]] && print_error "BLACKHOLE is not set" && exit 1
+[[ -z $PROCESSED_FOLDER ]] && print_error "PROCESSED_FOLDER is not set" && exit 1
 [[ -z $PUTIO_TOKEN ]] && print_error "PUTIO_TOKEN is not set" && exit 1
 [[ -z $FOLDER_ID ]] && print_error "FOLDER_ID is not set" && exit 1
 
@@ -13,7 +14,6 @@ do
 	result=$(curl --silent -F file=@${file} \
 		"https://upload.put.io/v2/files/upload?oauth_token=${PUTIO_TOKEN}&parent_id=${FOLDER_ID}" \
 	)
-#	jq . <<< $result
 
 	status=$(jq -r .status <<< $result)
 	if [[ $status == ERROR ]]; then
@@ -26,11 +26,8 @@ do
 	id=$(jq -r .transfer.id <<< $result)
 	print_success "Transfer added with id $id"
 
-	# Creating file for transfer tracking
-	touch $BLACKHOLE/${id}.transfer
-
-	# Marking torrent as processed
-	mv $file ${file}.$(date +"%Y-%m-%d_%H-%M").processed
+	# moving torrent to processed folder
+	mv $file $PROCESSED_FOLDER/$(basename ${file}).$(date +"%Y-%m-%d_%H-%M").${id}.processed
 
 	if [[ -n $SLACK_WEBHOOK_URL ]]; then
 		print_info "Sending notification to Slack"
@@ -40,52 +37,4 @@ do
 
 done
 
-print_info "Retrieving completed transfers list ..."
-completedTransfersList=$(curl --silent "https://api.put.io/v2/transfers/list?oauth_token=${PUTIO_TOKEN}")
-
-declare -a retrieveList
-oIFS=$IFS
-IFS=$'\n'
-for line in $(jq -r '.transfers[] | select(.status == "SEEDING" or .status == "COMPLETED")  | (.id | tostring) + ":" + .name + ":" + (.file_id | tostring)' <<< $completedTransfersList)
-do
-	dlId=$(cut -d':' -f1 <<< $line)
-	dlName=$(cut -d':' -f2 <<< $line)
-	fileId=$(cut -d':' -f3 <<< $line)
-
-	if [[ -f  $BLACKHOLE/${dlId}.transfer ]]; then
-		print_info "    - ${BOLD}$dlId${END} : ${GREEN}$dlName${END}"
-
-		# Get file type (folder or file)
-		dlType=$(curl --silent "https://api.put.io/v2/files/${fileId}?oauth_token=${PUTIO_TOKEN}" | jq -r .file.file_type)
-
-		# Building triggers files for jenkins jobs
-		if [[ $dlType == FOLDER ]]; then
-			print_info "        -> $dlId is a folder, zip needed"
-			zipResult=$(curl -XPOST --silent --data-urlencode "file_ids=$fileId" "https://api.put.io/v2/zips/create?oauth_token=${PUTIO_TOKEN}")
-
-			zipStatus=$(jq -r '.status' <<< $zipResult)
-			zipId=$(jq -r '.zip_id' <<< $zipResult)
-			if [[ $zipStatus == OK ]]; then
-				print_info "        -> Zip creation triggered, id $zipId"
-				
-				echo "ZIP_ID=$zipId" > ${dlId}.dlzip
-				echo "DESTINATION_FOLDER=$BLACKHOLE" >> ${dlId}.dlzip
-				echo "PUTIO_TOKEN=$PUTIO_TOKEN" >> ${dlId}.dlzip
-
-				mv $BLACKHOLE/${dlId}.transfer $BLACKHOLE/${dlId}.zipped
-			else
-				print_error "        -> Zip creation failed on put.io"
-			fi
-		else
-			print_info "    - Triggering download job for $dlId"
-			echo "URL=http://api.put.io/v2/files/${fileId}/download?oauth_token=${PUTIO_TOKEN}" > ${dlId}.dl
-			echo "DESTINATION_FOLDER=$BLACKHOLE" >> ${dlId}.dl
-			mv $BLACKHOLE/${dlId}.transfer $BLACKHOLE/${dlId}.downloaded
-		fi
-	else
-		print_info "    - ${BOLD}$dlId${END} : ${YELLOW}$dlName${END}"
-	fi
-
-done
-IFS=$oIFS
 
